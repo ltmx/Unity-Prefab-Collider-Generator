441de757ec29c4d696ffcdc32f7c77c31ad71066
LTMX <47640688+LTMX@users.noreply.github.com>
2020-06-01 15:35:16 +0200

    [Initial Commit]
    Added - Collider Bounds Debugger Script
    Added - Prefab Collider Generator Editor Script
    Added  - Prefab Collider Generator Script

---
+++ Unity Prefab Collider Generator v1.0/ColliderBoundsDebugger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Unity_Prefab_Collider_Generator_v1._0
+{
+    public class ColliderBoundsDebugger : MonoBehaviour
+    {
+        public Collider myCollider;
+
+        private void OnEnable() => myCollider = GetComponent<Collider>();
+
+        private void OnDrawGizmos()
+        {
+            if (myCollider == null) return;
+            var bounds = myCollider.bounds;
+            Gizmos.DrawWireCube(bounds.center, bounds.size);
+        }
+    }
+}
